60c048bef0a33034a05063f0a4915ebf52b7a051
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-05-21 12:11:57 +0200

    Add script

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdout.write('Write /version for Node version info, /language for user language info, /exit to exit program.\n');
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+  if(input !== null) {
+    var instruction = input.toString().trim();
+    switch (instruction) {
+      case '/version':
+        process.stdout.write(process.versions.node + '\n');
+        break;
+      case '/language':
+        process.stdout.write(process.env.lang + '\n');
+        break;
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    }
+  }
+});
